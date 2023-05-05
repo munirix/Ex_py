0f9bf50c157ec2dd900f99fca965785e8c1bc8e5
munirix <126810440+munirix@users.noreply.github.com>
2023-05-04 22:55:43 -0300

    Delete Listas directory

--- Listas/Ex1.py
+++
@@ -1,5 +0,0 @@
-#exercicio 1
-
-vetor = (input("Digite um vetor com cinco valores separados por vírgula ou espaços(ex: 2,54,7,90,12)\n "))
-#vetor = int(vetor[0:9:2])
-print("Os valores são: ", vetor)
--- Listas/Ex2.py
+++
@@ -1,4 +0,0 @@
-#exercicio 2
-
-vetor = (input("Digitee um vetor com dez valores: "))
-print("Os valores na ordem inversa são: ", reversed(vetor))
