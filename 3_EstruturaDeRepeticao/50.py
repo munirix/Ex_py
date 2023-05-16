c52f07cfe1c1a10f86aade853a726bcaa84fa8e2
munirix <126810440+munirix@users.noreply.github.com>
2023-05-16 19:39:36 -0300

    Update 50.py

--- 3_EstruturaDeRepeticao/50.py
+++ 3_EstruturaDeRepeticao/50.py
@@ -5,8 +5,8 @@ Faça um programa que calcule o valor de H com N termos.
 '''
 
 n = int(input('Digite o número: '))
-h = 1
+h = 0
 for i in range(1, n + 1):
-    h += h / i
+    h += 1 / i
 print('H: %.2f' %h)
     
