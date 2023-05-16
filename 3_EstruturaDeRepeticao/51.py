e778e20222b2d79cba07e1e214001c5db512389e
munirix <126810440+munirix@users.noreply.github.com>
2023-05-16 19:46:37 -0300

    Update 51.py

--- 3_EstruturaDeRepeticao/51.py
+++ 3_EstruturaDeRepeticao/51.py
@@ -7,8 +7,13 @@ Imprima no final a soma da série.
 n = int(input('Digite o número: '))
 s = 0
 d = 1
+print('S=',end=' ')
 for i in range(1, n + 1):
+    if i == n:
+        print(f'{i}/{d}',end=' ')
+    else:
+        print(f'{i}/{d}',end=' + ')
     s += i / d
     d += 2
-print('Soma da sequência: %.2f' % s)
+print('\nSoma da sequência: %.2f' % s)
     
