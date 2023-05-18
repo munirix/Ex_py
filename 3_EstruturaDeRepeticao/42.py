b9f4a6402e71e6db1eb99ba0d1c507fac08f0d5c
munirix <126810440+munirix@users.noreply.github.com>
2023-05-18 16:50:33 -0300

    Update 42.py

--- 3_EstruturaDeRepeticao/42.py
+++ 3_EstruturaDeRepeticao/42.py
@@ -4,6 +4,7 @@ números positivos e conte quantos deles estão
 nos seguintes intervalos: [0-25], [26-50], [51-75] e [76-100].
 A entrada de dados deverá terminar quando for lido
 um número negativo.
+ok
 '''
 
 n = 0
