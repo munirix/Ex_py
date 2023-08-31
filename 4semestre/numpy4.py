bde640b879f08026466c09e462835f5a7be6a36b
munirix <126810440+munirix@users.noreply.github.com>
2023-08-31 20:35:04 -0300

    Update numpy4.py
    boa

--- 4semestre/numpy4.py
+++ 4semestre/numpy4.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 arr = np.array(range(10))
-impar = np.where(arr % 2 != 0)
+#impar = np.where(arr % 2 != 0) -> (array([1, 3, 5, 7, 9], dtype=int64),)
 
-print(impar)
+print(arr[arr % 2 != 0]) # -> [1 3 5 7 9]
