8378994e2d3b3974b6ad1c7ef7a089a6f988afbb
munirix <126810440+munirix@users.noreply.github.com>
2023-08-31 20:13:01 -0300

    Create numpy3.py
    ver se existe outra forma

---
+++ 4semestre/numpy3.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#matriz11 = np.arange(True).reshape(3,3)
+x =  np.zeros((3,3))
+u = x >= 0
+print('\n----------------------------\n')
+print(x)
+print('\n----------------------------\n')
+print(u)
+print('\n----------------------------\n')
