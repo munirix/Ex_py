a6a0442beae60d85d144441b8bc2e1ebd5cd53e6
munirix <126810440+munirix@users.noreply.github.com>
2023-09-04 16:28:33 -0300

    Update numpy10.py

--- 4semestre/numpy10.py
+++ 4semestre/numpy10.py
@@ -1,10 +1,9 @@
 import numpy as np
 
-a = np.array([1,2,3])
-
-x = np.repeat(a,3)
-x = np.hstack((x,a))
-arr = np.append(np.repeat(a,3),a)
-print(x)
+a = np.array([1, 2, 3])
+# tile -> 123 123 123
+var_tile = np.tile(a, 3)
+# repeat -> 111 222 333
+var_repeat = np.repeat(a, 3)
+arr = np.append(var_repeat,var_tile)
 print(arr)
-
