acf99c1aa1cda44c1958b9d0f210ecd2d743cdd0
munirix <126810440+munirix@users.noreply.github.com>
2023-09-05 14:17:52 -0300

    criacao e finalizacao numpy13.py

---
+++ 4semestre/numpy13.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+A = np.array([12,9,4,1,11,5,8,1,1,2,3,1]).reshape(3,-1)
+print("A = \n",A)
+B = np.array([1,5,1,7,1,9,1,1]).reshape(4,-1)
+print("B = \n",B)
+
+C = np.dot(A,B)
+print("C = \n",C)
+
+def media(matriz):
+    print("\n"+"Função Média".center(50,"-")+"\n")
+    print("matriz:\n",matriz)
+
+    linha,coluna = matriz.shape
+
+    # SOMA E MEDIA DE CADA COLUNA DE UMA MATRIZ
+    soma_cada_coluna = matriz.sum(axis = 0)
+    media_cada_coluna = soma_cada_coluna/linha
+    media_cada_coluna = np.around(media_cada_coluna,2)
+    print("\nMédia de cada coluna: ",media_cada_coluna)
+    
+    # SOMA E MEDIA DOS ELEMENTOS DAS LINHAS DE UMA MATRIZ
+    soma_cada_linha = matriz.sum(axis = 1)
+    media_cada_linha = soma_cada_linha/coluna
+    media_cada_linha = np.around(media_cada_linha,2)
+    print("\nMédia de cada linha: ",media_cada_linha)
+
+    print("\n"+"Fim Função Média".center(50,"-")+"\n")
+
+def desvio_padrao(matriz):
+    print("\n"+"Função Desvio Padrão".center(50,"-")+"\n")
+    print("matriz:\n",matriz)
+
+    # desvio padrão ao longo da coluna
+    dp_coluna = np.around(np.std(matriz, axis=0))
+    print("\nDesvio padrão da coluna:", dp_coluna)
+    # desvio padrão ao longo da linha
+    dp_linha = np.around(np.std(matriz, axis=1))
+    print("\nDesvio padrão da linha:", dp_linha)
+
+    print("\n"+"Fim Função Desvio Padrão".center(50,"-")+"\n")
+
+matriz_media_C = media(C)
+matriz_dp_C = desvio_padrao(C)
